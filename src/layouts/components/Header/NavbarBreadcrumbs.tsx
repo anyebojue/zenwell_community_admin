c98ffbf4f59da3406dccbf80ea7a960a94f7f15f
WhiteFox <fei.su@zenwell.cn>
2024-12-03 09:42:06 +0800

    feat: 平台端面包屑

--- src/layouts/components/Header/NavbarBreadcrumbs.tsx
+++ src/layouts/components/Header/NavbarBreadcrumbs.tsx
@@ -1,6 +1,8 @@
 import { memo } from 'react'
+import getAllRoutes from 'routes'
 import { styled, Typography, Breadcrumbs, breadcrumbsClasses } from '@mui/material'
 import { NavigateNextRounded } from '@mui/icons-material'
+import { useLocation } from 'react-router-dom'
 
 const StyledBreadcrumbs = styled(Breadcrumbs)(({ theme }) => ({
   margin: theme.spacing(1, 0),
@@ -14,11 +16,14 @@ const StyledBreadcrumbs = styled(Breadcrumbs)(({ theme }) => ({
 }))
 
 const NavbarBreadcrumbs = () => {
+  const location = useLocation()
+  const path = getAllRoutes.filter(item => item.path === location.pathname)[0].meta?.title
+  console.log(path)
   return (
     <StyledBreadcrumbs aria-label="breadcrumb" separator={<NavigateNextRounded fontSize="small" />}>
-      <Typography variant="body1">Dashboard</Typography>
+      <Typography variant="body1">平台端</Typography>
       <Typography variant="body1" sx={{ color: 'text.primary', fontWeight: 600 }}>
-        Home
+        {path}
       </Typography>
     </StyledBreadcrumbs>
   )
