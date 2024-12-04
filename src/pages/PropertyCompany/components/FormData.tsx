b868907e61791a25e6e1bd23bfbaab6fb8518373
WhiteFox <fei.su@zenwell.cn>
2024-12-04 17:57:09 +0800

    feat: 优化表格导致的切换菜单卡顿的问题

--- src/pages/PropertyCompany/components/FormData.tsx
+++ src/pages/PropertyCompany/components/FormData.tsx
@@ -1,6 +1,6 @@
 import { memo } from 'react'
-import { Button, FormControl, Stack, TextField } from '@mui/material'
-import { Search, History, Add, Delete } from '@mui/icons-material'
+import { Box, FormControl, Button, Stack, TextField } from '@mui/material'
+import { Add, Delete } from '@mui/icons-material'
 
 const textFieldStyles = {
   '& .MuiOutlinedInput-root': {
@@ -27,67 +27,37 @@ const buttonStyles = (backgroundColor: string, hoverColor: string) => ({
 
 const FormData = () => {
   return (
-    <Stack direction="row" spacing={3} component="form" sx={{ mt: 3, mb: 1.5 }}>
-      <FormControl sx={{ width: { xs: '100%', md: '25ch' } }} variant="outlined">
-        <TextField
-          size="small"
-          label="请输入物业编号"
-          type="text"
-          variant="outlined"
-          sx={textFieldStyles}
-        />
-      </FormControl>
-      <FormControl sx={{ width: { xs: '100%', md: '25ch' } }} variant="outlined">
-        <TextField
-          size="small"
-          label="请输入名称"
-          type="text"
-          variant="outlined"
-          sx={{
-            '& .MuiOutlinedInput-root': {
-              '& fieldset': {
-                border: 'none' // 默认没有边框
-              },
-              '&:hover fieldset': {
-                border: '1px solid', // hover 时显示边框
-                borderColor: 'primary.main' // 使用主题的主色
-              },
-              '&.Mui-focused fieldset': {
-                border: '2px solid', // focus 时显示边框
-                borderColor: 'primary.main' // 使用主题的主色
-              }
-            }
-          }}
-        />
-      </FormControl>
-      <FormControl sx={{ width: { xs: '100%', md: '25ch' } }} variant="outlined">
-        <TextField
-          size="small"
-          label="请输入电话"
-          type="number"
-          variant="outlined"
-          sx={textFieldStyles}
-        />
-      </FormControl>
-      <Stack direction="row" spacing={1} component="form" sx={{ mt: 3, mb: 1.5 }}>
-        <Button
-          size="small"
-          variant="contained"
-          color="error"
-          startIcon={<Search />}
-          sx={buttonStyles('#2660ad', '#1d428a')}
-        >
-          查询
-        </Button>
-        <Button
-          size="small"
-          variant="contained"
-          color="error"
-          startIcon={<History />}
-          sx={buttonStyles('darkgray', '#696969')}
-        >
-          重置
-        </Button>
+    <Box>
+      <Stack direction="row" spacing={3} component="form" sx={{ mt: 3, mb: 1.5 }}>
+        <FormControl sx={{ width: { xs: '100%', md: '25ch' } }} variant="outlined">
+          <TextField
+            size="small"
+            label="请输入物业编号"
+            type="text"
+            variant="outlined"
+            sx={textFieldStyles}
+          />
+        </FormControl>
+        <FormControl sx={{ width: { xs: '100%', md: '25ch' } }} variant="outlined">
+          <TextField
+            size="small"
+            label="请输入名称"
+            type="text"
+            variant="outlined"
+            sx={textFieldStyles}
+          />
+        </FormControl>
+        <FormControl sx={{ width: { xs: '100%', md: '25ch' } }} variant="outlined">
+          <TextField
+            size="small"
+            label="请输入电话"
+            type="number"
+            variant="outlined"
+            sx={textFieldStyles}
+          />
+        </FormControl>
+      </Stack>
+      <Stack direction="row" spacing={1} component="form" sx={{ mb: 2 }}>
         <Button
           size="small"
           variant="contained"
@@ -107,7 +77,7 @@ const FormData = () => {
           批量删除
         </Button>
       </Stack>
-    </Stack>
+    </Box>
   )
 }
 
--- src/pages/PropertyCompany/components/gridData.tsx
+++ src/pages/PropertyCompany/components/gridData.tsx
@@ -1,317 +1,94 @@
 import { Box, Chip, Tooltip, IconButton } from '@mui/material'
-import { Block, Delete, Edit, ManageAccounts, RestartAlt, Login } from '@mui/icons-material'
-import { GridRowsProp, GridColDef } from '@mui/x-data-grid'
+import { Delete, Edit, ManageAccounts, Login, Block, RestartAlt } from '@mui/icons-material'
+import { ReactNode } from 'react'
 
-const renderSparklineCell = () => {
-  return (
-    <Box>
-      <Tooltip title="管理小区">
-        <IconButton size="small" color="primary">
-          <ManageAccounts fontSize="small" />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title="修改">
-        <IconButton size="small" color="secondary">
-          <Edit fontSize="small" />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title="删除">
-        <IconButton size="small" color="error">
-          <Delete fontSize="small" />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title="登录">
-        <IconButton size="small" color="success">
-          <Login fontSize="small" />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title="限制登录">
-        <IconButton size="small" color="warning">
-          <Block fontSize="small" />
-        </IconButton>
-      </Tooltip>
-      <Tooltip title="重置密码">
-        <IconButton size="small" color="info">
-          <RestartAlt fontSize="small" />
-        </IconButton>
-      </Tooltip>
-    </Box>
-  )
-}
-
-const renderStatus = (status: '审核完成' | 'Offline') => {
-  const colors: { [index: string]: 'success' | 'default' } = {
+const renderStatusChip = (value: string | undefined) => {
+  const colors: { [key: string]: 'success' | 'default' } = {
     审核完成: 'success',
     Offline: 'default'
   }
-  return <Chip label={status} color={colors[status]} size="small" />
+  return <Chip label={value} color={colors[value || '']} size="small" />
 }
 
-export const columns: GridColDef[] = [
-  { field: 'id', headerName: '小区ID', flex: 0.5, minWidth: 50 },
-  { field: 'name', headerName: '小区名称', flex: 1, minWidth: 100 },
-  { field: 'position', headerName: '附近地标', flex: 1, minWidth: 100 },
-  { field: 'cityCode', headerName: '城市编码', flex: 1, minWidth: 100 },
-  { field: 'communityCode', headerName: '社区编码', flex: 0.5, minWidth: 100 },
+const ActionColumn = (): ReactNode => (
+  <Box>
+    <Tooltip title="管理小区">
+      <IconButton size="small" color="primary">
+        <ManageAccounts fontSize="small" />
+      </IconButton>
+    </Tooltip>
+    <Tooltip title="修改">
+      <IconButton size="small" color="secondary">
+        <Edit fontSize="small" />
+      </IconButton>
+    </Tooltip>
+    <Tooltip title="删除">
+      <IconButton size="small" color="error">
+        <Delete fontSize="small" />
+      </IconButton>
+    </Tooltip>
+    <Tooltip title="登录">
+      <IconButton size="small" color="success">
+        <Login fontSize="small" />
+      </IconButton>
+    </Tooltip>
+    <Tooltip title="限制登录">
+      <IconButton size="small" color="warning">
+        <Block fontSize="small" />
+      </IconButton>
+    </Tooltip>
+    <Tooltip title="重置密码">
+      <IconButton size="small" color="info">
+        <RestartAlt fontSize="small" />
+      </IconButton>
+    </Tooltip>
+  </Box>
+)
+
+interface RowData {
+  id: string
+  name: string
+  position: string
+  cityCode: string
+  communityCode: string
+  status: string
+  operate: string
+}
+
+interface Column<T> {
+  headerName: string
+  key: keyof T
+  align?: 'left' | 'right' | 'center'
+  // eslint-disable-next-line no-unused-vars
+  renderCell?: (value: T[keyof T]) => ReactNode
+}
+
+export const columns: Column<RowData>[] = [
+  { key: 'id', headerName: '小区ID', align: 'center' },
+  { key: 'name', headerName: '小区名称', align: 'center' },
+  { key: 'position', headerName: '附近地标', align: 'center' },
+  { key: 'cityCode', headerName: '城市编码', align: 'center' },
+  { key: 'communityCode', headerName: '社区编码', align: 'center' },
   {
-    field: 'status',
+    key: 'status',
     headerName: '状态',
-    headerAlign: 'center',
     align: 'center',
-    flex: 1,
-    minWidth: 100,
-    renderCell: params => renderStatus(params.value as any)
+    renderCell: value => renderStatusChip(value)
   },
   {
-    field: 'operation',
+    key: 'operate',
     headerName: '操作',
-    headerAlign: 'center',
     align: 'center',
-    flex: 1,
-    minWidth: 250,
-    renderCell: renderSparklineCell
+    renderCell: () => <ActionColumn />
   }
 ]
 
-export const rows: GridRowsProp = [
-  {
-    id: '1',
-    name: 'WZ万洲小区',
-    position: '五四广场',
-    cityCode: '山东省青岛市市南区',
-    communityCode: '25555321',
-    status: '审核完成'
-  },
-  {
-    id: '2',
-    name: '东方化工厂',
-    position: '东方小区',
-    cityCode: '湖北省襄阳市襄城区	',
-    communityCode: '441000',
-    status: '审核完成'
-  },
-  {
-    id: '3',
-    name: '梯田金科花园	',
-    position: '成都市武侯区梯田路1号',
-    cityCode: '四川省成都市武侯区	',
-    communityCode: 'TTJK0036',
-    status: '审核完成'
-  },
-  {
-    id: '4',
-    name: 'WZ万洲小区',
-    position: '五四广场',
-    cityCode: '山东省青岛市市南区',
-    communityCode: '25555321',
-    status: '审核完成'
-  },
-  {
-    id: '5',
-    name: '梯田金科花园	',
-    position: '成都市武侯区梯田路1号',
-    cityCode: '四川省成都市武侯区	',
-    communityCode: 'TTJK0036',
-    status: '审核完成'
-  },
-  {
-    id: '6',
-    name: 'WZ万洲小区',
-    position: '五四广场',
-    cityCode: '山东省青岛市市南区',
-    communityCode: '25555321',
-    status: '审核完成'
-  },
-  {
-    id: '7',
-    name: '东方化工厂',
-    position: '东方小区',
-    cityCode: '湖北省襄阳市襄城区	',
-    communityCode: '441000',
-    status: '审核完成'
-  },
-  {
-    id: '8',
-    name: '梯田金科花园	',
-    position: '成都市武侯区梯田路1号',
-    cityCode: '四川省成都市武侯区	',
-    communityCode: 'TTJK0036',
-    status: '审核完成'
-  },
-  {
-    id: '9',
-    name: 'WZ万洲小区',
-    position: '五四广场',
-    cityCode: '山东省青岛市市南区',
-    communityCode: '25555321',
-    status: '审核完成'
-  },
-  {
-    id: '10',
-    name: '东方化工厂',
-    position: '东方小区',
-    cityCode: '湖北省襄阳市襄城区	',
-    communityCode: '441000',
-    status: '审核完成'
-  },
-  {
-    id: '11',
-    name: 'WZ万洲小区',
-    position: '五四广场',
-    cityCode: '山东省青岛市市南区',
-    communityCode: '25555321',
-    status: '审核完成'
-  },
-  {
-    id: '12',
-    name: '东方化工厂',
-    position: '东方小区',
-    cityCode: '湖北省襄阳市襄城区	',
-    communityCode: '441000',
-    status: '审核完成'
-  },
-  {
-    id: '13',
-    name: '梯田金科花园	',
-    position: '成都市武侯区梯田路1号',
-    cityCode: '四川省成都市武侯区	',
-    communityCode: 'TTJK0036',
-    status: '审核完成'
-  },
-  {
-    id: '14',
-    name: 'WZ万洲小区',
-    position: '五四广场',
-    cityCode: '山东省青岛市市南区',
-    communityCode: '25555321',
-    status: '审核完成'
-  },
-  {
-    id: '15',
-    name: '东方化工厂',
-    position: '东方小区',
-    cityCode: '湖北省襄阳市襄城区	',
-    communityCode: '441000',
-    status: '审核完成'
-  },
-  {
-    id: '16',
-    name: '梯田金科花园	',
-    position: '成都市武侯区梯田路1号',
-    cityCode: '四川省成都市武侯区	',
-    communityCode: 'TTJK0036',
-    status: '审核完成'
-  },
-  {
-    id: '17',
-    name: 'WZ万洲小区',
-    position: '五四广场',
-    cityCode: '山东省青岛市市南区',
-    communityCode: '25555321',
-    status: '审核完成'
-  },
-  {
-    id: '18',
-    name: '东方化工厂',
-    position: '东方小区',
-    cityCode: '湖北省襄阳市襄城区	',
-    communityCode: '441000',
-    status: '审核完成'
-  },
-  {
-    id: '19',
-    name: '梯田金科花园	',
-    position: '成都市武侯区梯田路1号',
-    cityCode: '四川省成都市武侯区	',
-    communityCode: 'TTJK0036',
-    status: '审核完成'
-  },
-  {
-    id: '20',
-    name: 'WZ万洲小区',
-    position: '五四广场',
-    cityCode: '山东省青岛市市南区',
-    communityCode: '25555321',
-    status: '审核完成'
-  },
-  {
-    id: '21',
-    name: '东方化工厂',
-    position: '东方小区',
-    cityCode: '湖北省襄阳市襄城区	',
-    communityCode: '441000',
-    status: '审核完成'
-  },
-  {
-    id: '22',
-    name: '梯田金科花园	',
-    position: '成都市武侯区梯田路1号',
-    cityCode: '四川省成都市武侯区	',
-    communityCode: 'TTJK0036',
-    status: '审核完成'
-  },
-  {
-    id: '23',
-    name: 'WZ万洲小区',
-    position: '五四广场',
-    cityCode: '山东省青岛市市南区',
-    communityCode: '25555321',
-    status: '审核完成'
-  },
-  {
-    id: '24',
-    name: '东方化工厂',
-    position: '东方小区',
-    cityCode: '湖北省襄阳市襄城区	',
-    communityCode: '441000',
-    status: '审核完成'
-  },
-  {
-    id: '25',
-    name: '梯田金科花园	',
-    position: '成都市武侯区梯田路1号',
-    cityCode: '四川省成都市武侯区	',
-    communityCode: 'TTJK0036',
-    status: '审核完成'
-  },
-  {
-    id: '26',
-    name: 'WZ万洲小区',
-    position: '五四广场',
-    cityCode: '山东省青岛市市南区',
-    communityCode: '25555321',
-    status: '审核完成'
-  },
-  {
-    id: '27',
-    name: '东方化工厂',
-    position: '东方小区',
-    cityCode: '湖北省襄阳市襄城区	',
-    communityCode: '441000',
-    status: '审核完成'
-  },
-  {
-    id: '28',
-    name: '梯田金科花园	',
-    position: '成都市武侯区梯田路1号',
-    cityCode: '四川省成都市武侯区	',
-    communityCode: 'TTJK0036',
-    status: '审核完成'
-  },
-  {
-    id: '29',
-    name: 'WZ万洲小区',
-    position: '五四广场',
-    cityCode: '山东省青岛市市南区',
-    communityCode: '25555321',
-    status: '审核完成'
-  },
-  {
-    id: '30',
-    name: '东方化工厂',
-    position: '东方小区',
-    cityCode: '湖北省襄阳市襄城区	',
-    communityCode: '441000',
-    status: '审核完成'
-  }
-]
+export const rows: RowData[] = Array.from({ length: 30 }, (_, index) => ({
+  id: String(index + 1),
+  name: index % 2 === 0 ? 'WZ万洲小区' : '东方化工厂',
+  position: index % 3 === 0 ? '五四广场' : '东方小区',
+  cityCode: index % 2 === 0 ? '山东省青岛市市南区' : '湖北省襄阳市襄城区',
+  communityCode: '25555321',
+  status: '审核完成',
+  operate: ''
+}))
