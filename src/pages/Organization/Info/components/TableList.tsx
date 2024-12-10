47345fa07e71f1945ddeee3f5a83f762fdea3a8f
WhiteFox <fei.su@zenwell.cn>
2024-12-10 15:24:36 +0800

    feat: [1001168]组织信息页面

---
+++ src/pages/Organization/Info/components/TableList.tsx
@@ -0,0 +1,195 @@
+import { memo, useState, useMemo, ChangeEvent } from 'react'
+import {
+  Pagination,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Select,
+  MenuItem,
+  Typography,
+  Box,
+  Paper,
+  SelectChangeEvent,
+  Checkbox,
+  Theme
+} from '@mui/material'
+import { CommunityReply } from 'api/model/communityModel'
+import { Column } from './TableData'
+
+const usePagination = <T,>(data: T[], rowsPerPage: number) => {
+  const [page, setPage] = useState(1)
+  const paginatedRows = useMemo(
+    () => data.slice((page - 1) * rowsPerPage, page * rowsPerPage),
+    [data, page, rowsPerPage]
+  )
+  const handlePageChange = (_: ChangeEvent<unknown>, value: number) => setPage(value)
+  return { page, paginatedRows, setPage, handlePageChange }
+}
+
+const TableList = ({
+  rows,
+  columns
+}: {
+  rows: CommunityReply[]
+  columns: Column<CommunityReply>[]
+}) => {
+  const [rowsPerPage, setRowsPerPage] = useState('20')
+  const { page, paginatedRows, setPage, handlePageChange } = usePagination(
+    rows,
+    Number(rowsPerPage)
+  )
+
+  const tableHeaderStyle = (theme: Theme) => ({
+    backgroundColor: theme.palette.action.hover,
+    '& th': {
+      fontWeight: 'bold',
+      borderBottom: `1px solid ${theme.palette.divider}`,
+      height: 45,
+      lineHeight: '45px'
+    }
+  })
+
+  const tableRowStyle = (theme: Theme) => ({
+    backgroundColor: theme.palette.mode === 'light' ? 'white' : theme.palette.background.paper,
+    '&:hover': {
+      backgroundColor: theme.palette.action.hover
+    }
+  })
+
+  const handleRowsPerPageChange = (event: SelectChangeEvent<string>) => {
+    setRowsPerPage(event.target.value)
+    setPage(1)
+  }
+
+  return (
+    <Box
+      sx={theme => ({
+        mt: 1,
+        width: '100%',
+        borderRadius: '7px',
+        border: `1px solid ${theme.palette.divider}`,
+        backgroundColor: theme.palette.background.paper
+      })}
+    >
+      <TableContainer
+        component={Paper}
+        sx={theme => ({
+          backgroundColor:
+            theme.palette.mode === 'light' ? 'white' : theme.palette.background.default
+        })}
+      >
+        <Table sx={{ minWidth: 650 }} aria-label="data table" size="small">
+          <TableHead>
+            <TableRow sx={tableHeaderStyle}>
+              <TableCell padding="checkbox">
+                <Checkbox color="primary" inputProps={{ 'aria-label': 'select all rows' }} />
+              </TableCell>
+              {columns.map(column => (
+                <TableCell
+                  key={column.headerName}
+                  align={column.align}
+                  sx={theme => ({
+                    fontWeight: 'bold',
+                    color: theme.palette.text.primary,
+                    borderBottom: `2px solid ${theme.palette.divider}`
+                  })}
+                >
+                  {column.headerName}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {paginatedRows.length > 0 ? (
+              paginatedRows.map(row => (
+                <TableRow key={row.id} sx={tableRowStyle}>
+                  <TableCell
+                    padding="checkbox"
+                    sx={{
+                      borderBottom: theme => `1px solid ${theme.palette.divider}`
+                    }}
+                  >
+                    <Checkbox
+                      color="primary"
+                      inputProps={{ 'aria-label': `select row ${row.id}` }}
+                    />
+                  </TableCell>
+                  {columns.map(column => {
+                    const value = row[column.key as keyof CommunityReply]
+                    return (
+                      <TableCell
+                        key={column.key}
+                        align={column.align}
+                        sx={{
+                          borderBottom: theme => `1px solid ${theme.palette.divider}`
+                        }}
+                      >
+                        {column.renderCell ? column.renderCell(value) : value}
+                      </TableCell>
+                    )
+                  })}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={columns.length + 1} align="center">
+                  暂无数据
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          margin: '10px 10px'
+        }}
+      >
+        <Typography variant="body2" sx={{ marginLeft: 2 }}>
+          共 {rows.length} 项数据
+        </Typography>
+        <Box sx={{ display: 'flex', alignItems: 'center' }}>
+          <Typography variant="body2" sx={{ marginRight: 1 }}>
+            每页行数:
+          </Typography>
+          <Select
+            value={rowsPerPage}
+            onChange={handleRowsPerPageChange}
+            size="small"
+            sx={{
+              marginRight: 2,
+              '.MuiOutlinedInput-notchedOutline': {
+                border: 'none'
+              },
+              '& .MuiSelect-select': {
+                padding: 0
+              }
+            }}
+          >
+            {[10, 20, 50].map(size => (
+              <MenuItem key={size} value={size}>
+                {size}
+              </MenuItem>
+            ))}
+          </Select>
+          <Pagination
+            count={Math.ceil(rows.length / Number(rowsPerPage))}
+            page={page}
+            onChange={handlePageChange}
+            color="primary"
+            shape="rounded"
+            size="small"
+          />
+        </Box>
+      </Box>
+    </Box>
+  )
+}
+
+export default memo(TableList)
--- src/pages/Organization/Info/index.tsx
+++ src/pages/Organization/Info/index.tsx
@@ -1,13 +1,119 @@
 import { memo } from 'react'
-import { Box } from '@mui/material'
+import { TreeViewBaseItem } from '@mui/x-tree-view/models'
+import { RichTreeView } from '@mui/x-tree-view/RichTreeView'
+import { Box, Button, Stack, Theme, Typography } from '@mui/material'
+import { Add, FileCopy } from '@mui/icons-material'
 import NavbarBreadcrumbs from 'layouts/components/Header/NavbarBreadcrumbs'
 import Copyright from 'layouts/components/Copyright'
+import { buttonStyles } from 'components/DeleteModal'
+import FormSearch from './components/FormSearch'
+import TableData from './components/TableData'
+
+const MUI_X_PRODUCTS: TreeViewBaseItem[] = [
+  {
+    id: '0',
+    label: 'WhiteFox物业',
+    children: [
+      { id: '001', label: '001小区' },
+      { id: '002', label: '002小区' },
+      { id: '003', label: '003小区' },
+      { id: '004', label: '004小区' },
+      { id: '005', label: '005小区' },
+      { id: '006', label: '006小区' },
+      { id: '007', label: '007小区' },
+      { id: '008', label: '008小区' },
+      { id: '009', label: '009小区' },
+      { id: '010', label: '010小区' }
+    ]
+  },
+  {
+    id: '1',
+    label: 'Zenwell物业',
+    children: [
+      { id: '011', label: '001小区' },
+      { id: '012', label: '002小区' },
+      { id: '013', label: '003小区' },
+      { id: '014', label: '004小区' },
+      { id: '015', label: '005小区' },
+      { id: '016', label: '006小区' },
+      { id: '017', label: '007小区' },
+      { id: '018', label: '008小区' },
+      { id: '019', label: '009小区' },
+      { id: '020', label: '010小区' }
+    ]
+  },
+  {
+    id: '2',
+    label: '测试物业',
+    children: [
+      { id: '021', label: '001小区' },
+      { id: '022', label: '002小区' },
+      { id: '023', label: '003小区' },
+      { id: '024', label: '004小区' },
+      { id: '025', label: '005小区' },
+      { id: '026', label: '006小区' },
+      { id: '027', label: '007小区' },
+      { id: '028', label: '008小区' },
+      { id: '029', label: '009小区' },
+      { id: '030', label: '010小区' }
+    ]
+  }
+]
+
+const treeViewStyle = (theme: Theme) => ({
+  background: theme.palette.background.default,
+  borderRadius: '15px',
+  padding: '20px 15px',
+  width: '22%'
+})
+
+const contentBoxStyle = (theme: Theme) => ({
+  background: theme.palette.background.default,
+  borderRadius: '15px',
+  padding: '15px 15px',
+  width: '100%'
+})
 
 const InfoIndex = () => {
   return (
     <Box sx={{ mt: 3.5, width: '100%', height: '100%' }}>
       <NavbarBreadcrumbs />
-      InfoIndex
+      <Stack sx={{ mt: 2, mb: 1.5 }} direction="row" spacing={3}>
+        <RichTreeView
+          sx={treeViewStyle}
+          defaultExpandedItems={['0', '1', '2']}
+          items={MUI_X_PRODUCTS}
+        />
+        <Box sx={{ width: '100%' }}>
+          <FormSearch />
+          <Box sx={contentBoxStyle}>
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+              <Typography variant="h6">WhiteFox物业 员工</Typography>
+              <Stack direction="row" spacing={1}>
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="error"
+                  startIcon={<FileCopy />}
+                  sx={buttonStyles('#2660ad', '#1d428a')}
+                >
+                  文档
+                </Button>
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="error"
+                  startIcon={<Add />}
+                  sx={buttonStyles('#2660ad', '#1d428a')}
+                >
+                  关联员工
+                </Button>
+              </Stack>
+            </Box>
+            <TableData />
+          </Box>
+        </Box>
+      </Stack>
       <Copyright />
     </Box>
   )
