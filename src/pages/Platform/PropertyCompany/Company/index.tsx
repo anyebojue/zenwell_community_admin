94dfad93148f3d84ef8957ab150f051f6edbe5b6
WhiteFox <fei.su@zenwell.cn>
2024-12-19 16:56:27 +0800

    feat: 【ID1001149】[FE]入驻小区页面

--- src/pages/Platform/PropertyCompany/Company/index.tsx
+++ src/pages/Platform/PropertyCompany/Company/index.tsx
@@ -1,12 +1,55 @@
 import { memo } from 'react'
+import { Box, Button, Stack, Theme, Typography } from '@mui/material'
+import { Add, Close } from '@mui/icons-material'
+import NavbarBreadcrumbs from 'layouts/components/Header/NavbarBreadcrumbs'
+import Copyright from 'layouts/components/Copyright'
+import { buttonStyles } from 'components/DeleteModal'
+import FormSearch from './components/FormSearch'
+import TableData from './components/TableData'
 
-const CommunityIndex = () => {
+const contentBoxStyle = (theme: Theme) => ({
+  background: theme.palette.background.default,
+  borderRadius: '15px',
+  padding: '15px 15px',
+  width: '100%'
+})
+
+const CompanyIndex = () => {
   return (
-    <>
-      <div>111111</div>
-      <div></div>
-    </>
+    <Box sx={{ mt: 3.5, width: '100%', height: '100%' }}>
+      <NavbarBreadcrumbs />
+      <Box sx={{ width: '100%' }}>
+        <FormSearch />
+        <Box sx={contentBoxStyle}>
+          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+            <Typography variant="h6">小区信息</Typography>
+            <Stack direction="row" spacing={1}>
+              <Button
+                size="small"
+                variant="contained"
+                color="error"
+                startIcon={<Add />}
+                sx={buttonStyles('#2660ad', '#1d428a')}
+              >
+                加入小区
+              </Button>
+              <Button
+                size="small"
+                variant="contained"
+                color="error"
+                startIcon={<Close />}
+                sx={buttonStyles('#2660ad', '#1d428a')}
+              >
+                返回
+              </Button>
+            </Stack>
+          </Box>
+          <TableData />
+        </Box>
+      </Box>
+      <Copyright />
+    </Box>
   )
 }
 
-export default memo(CommunityIndex)
+export default memo(CompanyIndex)
