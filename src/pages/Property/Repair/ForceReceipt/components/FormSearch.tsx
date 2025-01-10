1418d0fd675a8a3117d2286d7704bbe08079f5f5
WhiteFox <fei.su@zenwell.cn>
2025-01-10 11:09:10 +0800

    feat: 强制回单

--- src/pages/Property/Repair/ForceReceipt/components/FormSearch.tsx
+++ src/pages/Property/Repair/ForceReceipt/components/FormSearch.tsx
@@ -1,7 +1,7 @@
 import { ChangeEvent, memo, useState, useCallback } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { RepairSettingParams } from 'api/model/property/repairSettingModel'
-import { find } from 'modules/property/repairSetting'
+import { RepairPoolParams } from 'api/model/property/repairPoolModel'
+import { find } from 'modules/property/repairPool'
 import { Box, FormControl, Button, Stack, TextField, MenuItem } from '@mui/material'
 import { History, Search } from '@mui/icons-material'
 import { buttonStyles } from 'components/DeleteModal'
@@ -27,18 +27,16 @@ interface SearchFormProps {}
 
 const FormSearch: React.FC<SearchFormProps> = () => {
   const dispatch = useDispatch<AppDispatch>()
-  const { page } = useSelector((state: RootState) => state.RepairSettingSlice)
+  const { page, list } = useSelector((state: RootState) => state.RepairSettingSlice)
 
-  const [searchParams, setSearchParams] = useState<RepairSettingParams>({
-    repairTypeName: '',
-    repairWay: 0,
-    repairType: '',
-    publicArea: 0,
-    returnVisitFlag: 0
+  const [searchParams, setSearchParams] = useState<RepairPoolParams>({
+    repairName: '',
+    tel: '',
+    repairSettingId: ''
   })
 
   const handleInputChange =
-    (field: keyof RepairSettingParams) => (event: ChangeEvent<HTMLInputElement>) => {
+    (field: keyof RepairPoolParams) => (event: ChangeEvent<HTMLInputElement>) => {
       setSearchParams(prevData => ({
         ...prevData,
         [field]: event.target.value
@@ -46,11 +44,11 @@ const FormSearch: React.FC<SearchFormProps> = () => {
     }
 
   const fetchData = useCallback(
-    async (params: RepairSettingParams & PaginationParams) => {
+    async (params: RepairPoolParams & PaginationParams) => {
       const closeLoading = message.loading('正在加载列表中，请稍后...')
       try {
         const res = await dispatch(
-          find({ 'page.num': page.num, 'page.size': page.size, ...params })
+          find({ 'page.num': page.num, 'page.size': page.size, ...params, statusCd: 1100 })
         )
         if ('error' in res && res.error?.message) {
           throw new Error(res.error.message)
@@ -79,8 +77,8 @@ const FormSearch: React.FC<SearchFormProps> = () => {
             type="text"
             variant="outlined"
             sx={textFieldStyles}
-            value={searchParams.repairTypeName}
-            onChange={handleInputChange('repairTypeName')}
+            value={searchParams.repairName}
+            onChange={handleInputChange('repairName')}
           />
         </FormControl>
         <FormControl sx={{ width: { xs: '100%', md: '25ch' } }} variant="outlined">
@@ -90,8 +88,8 @@ const FormSearch: React.FC<SearchFormProps> = () => {
             type="text"
             variant="outlined"
             sx={textFieldStyles}
-            value={searchParams.repairTypeName}
-            onChange={handleInputChange('repairTypeName')}
+            value={searchParams.tel}
+            onChange={handleInputChange('tel')}
           />
         </FormControl>
         <FormControl sx={{ width: { xs: '100%', md: '25ch' } }} variant="outlined">
@@ -99,14 +97,14 @@ const FormSearch: React.FC<SearchFormProps> = () => {
             select
             size="small"
             label="请选择报修类型"
-            value={searchParams.repairWay || ''}
-            onChange={handleInputChange('repairWay')}
+            value={searchParams.repairSettingId || ''}
+            onChange={handleInputChange('repairSettingId')}
             variant="outlined"
             sx={textFieldStyles}
           >
-            {[{ value: 100, label: '工单池派单' }].map(option => (
-              <MenuItem key={option.value} value={option.value}>
-                {option.label}
+            {list.map(option => (
+              <MenuItem key={option.id} value={option.id}>
+                {option.repairTypeName}
               </MenuItem>
             ))}
           </TextField>
@@ -130,18 +128,14 @@ const FormSearch: React.FC<SearchFormProps> = () => {
             sx={buttonStyles('darkgray', '#696969')}
             onClick={() => {
               setSearchParams({
-                repairTypeName: '',
-                repairWay: 0,
-                repairType: '',
-                publicArea: 0,
-                returnVisitFlag: 0
+                repairName: '',
+                tel: '',
+                repairSettingId: ''
               })
               fetchData({
-                repairTypeName: '',
-                repairWay: 0,
-                repairType: '',
-                publicArea: 0,
-                returnVisitFlag: 0,
+                repairName: '',
+                tel: '',
+                repairSettingId: '',
                 'page.num': page.num,
                 'page.size': page.size
               })
--- src/pages/Property/Repair/ForceReceipt/components/TableData.tsx
+++ src/pages/Property/Repair/ForceReceipt/components/TableData.tsx
@@ -1,7 +1,8 @@
 import { Dispatch, memo, ReactNode, SetStateAction, useCallback, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { RepairSettingReply } from 'api/model/property/repairSettingModel'
-import { find } from 'modules/property/repairSetting'
+import { RepairPoolReply } from 'api/model/property/repairPoolModel'
+import { find } from 'modules/property/repairPool'
+import { find as findRepairSetting } from 'modules/property/repairSetting'
 import { Box, Tooltip, IconButton } from '@mui/material'
 import { DoneAll, FileCopy } from '@mui/icons-material'
 import message from 'components/Message'
@@ -46,17 +47,50 @@ interface TableDataProps {
 
 const TableData: React.FC<TableDataProps> = () => {
   const dispatch = useDispatch<AppDispatch>()
-  const { page, list } = useSelector((state: RootState) => state.RepairSettingSlice)
+  const { page, list } = useSelector((state: RootState) => state.RepairPoolSlice)
 
-  const columns: Column<RepairSettingReply>[] = [
-    { key: 'repairTypeName', headerName: '工单编码', align: 'center' },
-    { key: 'repairTypeName', headerName: '位置', align: 'center' },
-    { key: 'repairTypeName', headerName: '报修类型', align: 'center' },
-    { key: 'repairTypeName', headerName: '报修人', align: 'center' },
-    { key: 'repairTypeName', headerName: '联系方式', align: 'center' },
-    { key: 'repairTypeName', headerName: '预约时间', align: 'center' },
-    { key: 'repairTypeName', headerName: '提交时间', align: 'center' },
-    { key: 'repairTypeName', headerName: '状态', align: 'center' },
+  const columns: Column<RepairPoolReply>[] = [
+    { key: 'id', headerName: '工单编码', align: 'center' },
+    { key: 'communityId', headerName: '位置', align: 'center' },
+    {
+      key: 'repairSetting',
+      headerName: '报修类型',
+      align: 'center',
+      renderCell: row => row.repairSetting?.repairTypeName
+    },
+    { key: 'repairName', headerName: '报修人', align: 'center' },
+    { key: 'tel', headerName: '联系方式', align: 'center' },
+    { key: 'appointmentTime', headerName: '预约时间', align: 'center' },
+    { key: 'createdAt', headerName: '提交时间', align: 'center' },
+    {
+      key: 'statusCd',
+      headerName: '状态',
+      align: 'center',
+      renderCell: row =>
+        row.statusCd === 1000
+          ? '未派单'
+          : row.statusCd === 1100
+            ? '接单'
+            : row.statusCd === 1200
+              ? '退单'
+              : row.statusCd === 1300
+                ? '转单'
+                : row.statusCd === 1400
+                  ? '申请支付'
+                  : row.statusCd === 1500
+                    ? '支付失败'
+                    : row.statusCd === 1700
+                      ? '待评价'
+                      : row.statusCd === 1800
+                        ? '电话回访'
+                        : row.statusCd === 1900
+                          ? '办理完成'
+                          : row.statusCd === 2000
+                            ? '未办理结单'
+                            : row.statusCd === 2001
+                              ? '暂停'
+                              : ''
+    },
     {
       key: 'operate',
       headerName: '操作',
@@ -80,9 +114,27 @@ const TableData: React.FC<TableDataProps> = () => {
     }
   }, [dispatch, page.num, page.size])
 
+  const fetchRepairSettingData = useCallback(async () => {
+    const closeLoading = message.loading('正在加载列表中，请稍后...')
+    try {
+      const res = await dispatch(
+        findRepairSetting({ 'page.num': page.num, 'page.size': page.size, statusCd: 1100 })
+      )
+      if ('error' in res && res.error?.message) {
+        throw new Error(res.error.message)
+      }
+    } catch (err: unknown) {
+      closeLoading()
+      if (err instanceof Error) message.error(err.message)
+    } finally {
+      closeLoading()
+    }
+  }, [dispatch, page.num, page.size])
+
   useEffect(() => {
     fetchData()
-  }, [fetchData])
+    fetchRepairSettingData()
+  }, [fetchData, fetchRepairSettingData])
 
   return <TableList rows={list} columns={columns} />
 }
