9432fa789df4a3e0d31057c2bcff94bdad4e98cf
WhiteFox <whitefox@WhiteFoxdeMacBook-Air.local>
2025-07-04 10:44:05 +0800

    feat: 1

--- src/pages/Property/Communitys/GoodsRelease/components/FormDialog.tsx
+++ src/pages/Property/Communitys/GoodsRelease/components/FormDialog.tsx
@@ -89,7 +89,9 @@ const FormDialog: React.FC<FormDialogProps> = ({
       event.preventDefault()
       setLoading(true)
       try {
-        const params = { ...formData }
+        const current_community = localStorage.getItem('current_community')
+        const community = JSON.parse(current_community || '')
+        const params = { ...formData, communityId: community?.id }
         const action =
           dialogType === 'add'
             ? create({ ...params, statusCd: 'W' })
