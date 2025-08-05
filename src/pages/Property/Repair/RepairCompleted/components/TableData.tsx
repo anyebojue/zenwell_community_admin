4d34cee1f14acd342684180b5f68b97d0a1af715
WhiteFox <sufei@zenwell.onaliyun.com>
2025-08-05 17:53:54 +0800

    feat: 报修已办

--- src/pages/Property/Repair/RepairCompleted/components/TableData.tsx
+++ src/pages/Property/Repair/RepairCompleted/components/TableData.tsx
@@ -1,25 +1,34 @@
-import { Dispatch, memo, ReactNode, SetStateAction, useCallback, useEffect } from 'react'
+import { memo, useCallback, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { RepairPoolReply } from 'api/model/property/repair/repairPoolModel'
 import { find } from 'modules/property/repair/repairPool'
 import { find as findRepairSetting } from 'modules/property/repair/repairSetting'
-import { Box, Tooltip, IconButton } from '@mui/material'
-import { FileCopy } from '@mui/icons-material'
+import { Chip } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
+import { zhCN } from '@mui/x-data-grid/locales'
 import message from 'components/Message'
 import { useNavigate } from 'react-router-dom'
-import TableList from './TableList'
 
-export interface Column<T> {
-  headerName: string
-  key: keyof T | 'operate'
-  align?: 'left' | 'center' | 'right'
-  renderCell?: (row: T) => ReactNode
+const statusType: Record<string, string> = {
+  1001: '有偿服务',
+  1002: '无偿服务'
 }
 
-interface TableDataProps {
-  selectedRows: Set<string | undefined>
-  setSelectedRows: Dispatch<SetStateAction<Set<string | undefined>>>
+const statusValue: Record<string, string> = {
+  1000: '未派单',
+  1100: '接单',
+  1200: '退单',
+  1300: '转单',
+  1400: '申请支付',
+  1500: '支付失败',
+  1700: '待评价',
+  1800: '电话回访',
+  1900: '办理完成',
+  2000: '未办理结单',
+  2001: '暂停'
 }
+
+interface TableDataProps {}
 
 const TableData: React.FC<TableDataProps> = () => {
   const dispatch = useDispatch<AppDispatch>()
@@ -28,119 +37,136 @@ const TableData: React.FC<TableDataProps> = () => {
   const current_community = localStorage.getItem('current_community')
   const community = JSON.parse(current_community || '')
 
-  const columns: Column<RepairPoolReply>[] = [
-    { key: 'id', headerName: '工单编号', align: 'center' },
-    { key: 'communityId', headerName: '位置', align: 'center', renderCell: () => community.name },
-    {
-      key: 'repairSetting',
-      headerName: '报修类型',
-      align: 'center',
-      renderCell: row => row.repairSetting?.repairTypeName
-    },
-    {
-      key: 'maintenanceType',
-      headerName: '维修类型',
-      align: 'center',
-      renderCell: row =>
-        row.maintenanceType === 1001 ? '有偿服务' : row.maintenanceType === 1002 ? '无偿服务' : ''
-    },
-    { key: 'repairName', headerName: '报修人', align: 'center' },
-    { key: 'tel', headerName: '联系方式', align: 'center' },
-    { key: 'appointmentTime', headerName: '预约时间', align: 'center' },
-    {
-      key: 'statusCd',
-      headerName: '状态',
-      align: 'center',
-      renderCell: row =>
-        row.statusCd === 1000
-          ? '未派单'
-          : row.statusCd === 1100
-            ? '接单'
-            : row.statusCd === 1200
-              ? '退单'
-              : row.statusCd === 1300
-                ? '转单'
-                : row.statusCd === 1400
-                  ? '申请支付'
-                  : row.statusCd === 1500
-                    ? '支付失败'
-                    : row.statusCd === 1700
-                      ? '待评价'
-                      : row.statusCd === 1800
-                        ? '电话回访'
-                        : row.statusCd === 1900
-                          ? '办理完成'
-                          : row.statusCd === 2000
-                            ? '未办理结单'
-                            : row.statusCd === 2001
-                              ? '暂停'
-                              : ''
-    },
-    {
-      key: 'operate',
-      headerName: '操作',
-      align: 'center',
-      renderCell: row => (
-        <Box>
-          {[
-            {
-              title: '详情',
-              color: 'primary' as const,
-              icon: <FileCopy fontSize="small" />,
-              onClick: () => navigate('/repair/work-order-details', { state: { value: row } })
-            }
-          ].map((action, index) => (
-            <Tooltip title={action.title} key={index}>
-              <IconButton size="small" color={action.color} onClick={action.onClick}>
-                {action.icon}
-              </IconButton>
-            </Tooltip>
-          ))}
-        </Box>
-      )
-    }
-  ]
-
-  const fetchData = useCallback(async () => {
-    const closeLoading = message.loading('正在加载列表中，请稍后...')
-    try {
-      const res = await dispatch(
-        find({ 'page.num': page.num, 'page.size': page.size, statusCd: 1900 })
-      )
-      if ('error' in res && res.error?.message) {
-        throw new Error(res.error.message)
+  const fetchData = useCallback(
+    async (action: Function, params: Record<string, boolean | string>, loadingMessage: string) => {
+      const closeLoading = message.loading(loadingMessage)
+      try {
+        const res = await dispatch(action(params))
+        if ('error' in res && res.error?.message) {
+          throw new Error(res.error.message)
+        }
+      } catch (err: unknown) {
+        if (err instanceof Error) message.error(err.message)
+      } finally {
+        closeLoading()
       }
-    } catch (err: unknown) {
-      closeLoading()
-      if (err instanceof Error) message.error(err.message)
-    } finally {
-      closeLoading()
-    }
-  }, [dispatch, page.num, page.size])
-
-  const fetchRepairSettingData = useCallback(async () => {
-    const closeLoading = message.loading('正在加载列表中，请稍后...')
-    try {
-      const res = await dispatch(
-        findRepairSetting({ 'page.num': page.num, 'page.size': page.size })
-      )
-      if ('error' in res && res.error?.message) {
-        throw new Error(res.error.message)
-      }
-    } catch (err: unknown) {
-      closeLoading()
-      if (err instanceof Error) message.error(err.message)
-    } finally {
-      closeLoading()
-    }
-  }, [dispatch, page.num, page.size])
+    },
+    [dispatch]
+  )
 
   useEffect(() => {
-    fetchData()
-    fetchRepairSettingData()
-  }, [fetchData, fetchRepairSettingData])
+    fetchData(find, { 'page.num': page.num, 'page.size': page.size }, '正在加载列表中，请稍后...')
+    fetchData(
+      findRepairSetting,
+      { 'page.num': page.num, 'page.size': page.size },
+      '正在加载列表中，请稍后...'
+    )
+  }, [fetchData, page.num, page.size])
 
-  return <TableList rows={list} columns={columns} />
+  const handleActionClick = useCallback(
+    (actionType: string, row: RepairPoolReply) => {
+      switch (actionType) {
+        case 'edit':
+          navigate('/repair/WorkOrderDetails', { state: { value: row } })
+          break
+      }
+    },
+    [navigate]
+  )
+
+  const renderActionButtons = (row: RepairPoolReply) =>
+    [{ title: '详情', action: 'edit' }].map(({ title, action }) => (
+      <Chip
+        key={title}
+        sx={{
+          cursor: 'pointer',
+          '& .MuiChip-label': {
+            fontSize: '13px'
+          }
+        }}
+        label={title}
+        color="primary"
+        variant="outlined"
+        onClick={() => handleActionClick(action, row)}
+      />
+    ))
+
+  return (
+    <DataGrid
+      sx={{ mt: 1 }}
+      localeText={zhCN.components.MuiDataGrid.defaultProps.localeText}
+      disableColumnResize
+      disableVirtualization={false}
+      rows={list}
+      columns={[
+        { field: 'id', headerName: '工单编号', flex: 1, headerAlign: 'center', align: 'center' },
+        {
+          field: 'communityId',
+          headerName: '位置',
+          flex: 1,
+          headerAlign: 'center',
+          align: 'center',
+          renderCell: () => community.name
+        },
+        {
+          field: 'repairSetting.repairTypeName',
+          headerName: '报修类型',
+          flex: 1,
+          headerAlign: 'center',
+          align: 'center',
+          renderCell: ({ row }) => row.repairSetting?.repairTypeName
+        },
+        {
+          field: 'maintenanceType',
+          headerName: '维修类型',
+          flex: 1,
+          headerAlign: 'center',
+          align: 'center',
+          renderCell: ({ row }) => <Chip label={statusType[row.maintenanceType!] || '未知'} />
+        },
+        {
+          field: 'repairName',
+          headerName: '报修人',
+          flex: 1,
+          headerAlign: 'center',
+          align: 'center'
+        },
+        { field: 'tel', headerName: '联系方式', flex: 1, headerAlign: 'center', align: 'center' },
+        {
+          field: 'appointmentTime',
+          headerName: '预约时间',
+          flex: 1,
+          headerAlign: 'center',
+          align: 'center'
+        },
+        {
+          field: 'statusCd',
+          headerName: '状态',
+          flex: 1,
+          headerAlign: 'center',
+          align: 'center',
+          renderCell: ({ row }) => <Chip label={statusValue[row.statusCd!] || '未知'} />
+        },
+        {
+          field: 'actions',
+          headerName: '操作',
+          type: 'actions',
+          width: 100,
+          getActions: ({ row }) => renderActionButtons(row)
+        }
+      ]}
+      pageSizeOptions={[10, 20, 50, 100]}
+      paginationMode="server"
+      rowCount={Number(page.total)}
+      initialState={{
+        pagination: {
+          paginationModel: {
+            pageSize: Number(page.size)
+          }
+        }
+      }}
+    />
+  )
 }
 
 export default memo(TableData)
--- src/pages/Property/Repair/RepairCompleted/index.tsx
+++ src/pages/Property/Repair/RepairCompleted/index.tsx
@@ -1,4 +1,4 @@
-import { memo, useState } from 'react'
+import { memo } from 'react'
 import { Box, Theme, Typography } from '@mui/material'
 import NavbarBreadcrumbs from 'layouts/components/Header/NavbarBreadcrumbs'
 import Copyright from 'layouts/components/Copyright'
@@ -13,19 +13,15 @@ const contentBoxStyle = (theme: Theme) => ({
 })
 
 const RepairSettingsIndex = () => {
-  const [selectedRows, setSelectedRows] = useState<Set<string | undefined>>(new Set())
-
   return (
     <Box sx={{ mt: 3.5, width: '100%', height: '100%' }}>
       <NavbarBreadcrumbs />
-      <Box sx={{ width: '100%' }}>
-        <FormSearch />
-        <Box sx={contentBoxStyle}>
-          <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
-            <Typography variant="h6">报修待办</Typography>
-          </Box>
-          <TableData selectedRows={selectedRows} setSelectedRows={setSelectedRows} />
+      <FormSearch />
+      <Box sx={contentBoxStyle}>
+        <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+          <Typography variant="h6">报修已办</Typography>
         </Box>
+        <TableData />
       </Box>
       <Copyright />
     </Box>
