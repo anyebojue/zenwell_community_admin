c546a8af1c40f67538f06a1196692db9e4966d94
WhiteFox <sufei@zenwell.onaliyun.com>
2025-08-05 17:38:14 +0800

    feat: 报修待办

--- src/pages/Property/Repair/RepairTodo/components/TableData.tsx
+++ src/pages/Property/Repair/RepairTodo/components/TableData.tsx
@@ -1,37 +1,34 @@
-import { Dispatch, memo, ReactNode, SetStateAction, useCallback, useEffect, useState } from 'react'
+import { memo, useCallback, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { RepairPoolReply } from 'api/model/property/repair/repairPoolModel'
 import { find } from 'modules/property/repair/repairPool'
 import { find as findRepairSetting } from 'modules/property/repair/repairSetting'
-import { Box, Tooltip, IconButton } from '@mui/material'
-import {
-  SwapHoriz,
-  Undo,
-  CheckCircle,
-  PauseCircle,
-  FileCopy,
-  PlayCircle
-} from '@mui/icons-material'
+import { Chip } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
+import { zhCN } from '@mui/x-data-grid/locales'
 import message from 'components/Message'
 import { useNavigate } from 'react-router-dom'
-import TableList from './TableList'
 import TransferOfOrder from './TransferOfOrder'
 import Chargeback from './Chargeback'
+import Conclude from './Conclude'
 import Pause from './Pause'
 import Launch from './Launch'
-import Conclude from './Conclude'
 
-export interface Column<T> {
-  headerName: string
-  key: keyof T | 'operate'
-  align?: 'left' | 'center' | 'right'
-  renderCell?: (row: T) => ReactNode
+const statusValue: Record<string, string> = {
+  1000: '未派单',
+  1100: '接单',
+  1200: '退单',
+  1300: '转单',
+  1400: '申请支付',
+  1500: '支付失败',
+  1700: '待评价',
+  1800: '电话回访',
+  1900: '办理完成',
+  2000: '未办理结单',
+  2001: '暂停'
 }
 
-interface TableDataProps {
-  selectedRows: Set<string | undefined>
-  setSelectedRows: Dispatch<SetStateAction<Set<string | undefined>>>
-}
+interface TableDataProps {}
 
 const TableData: React.FC<TableDataProps> = () => {
   const dispatch = useDispatch<AppDispatch>()
@@ -46,152 +43,161 @@ const TableData: React.FC<TableDataProps> = () => {
   const [activateOpen, setActivateOpen] = useState(false)
   const [pauseOpen, setPauseOpen] = useState(false)
 
-  const columns: Column<RepairPoolReply>[] = [
-    { key: 'communityId', headerName: '位置', align: 'center', renderCell: () => community.name },
-    {
-      key: 'repairSetting',
-      headerName: '报修类型',
-      align: 'center',
-      renderCell: row => row.repairSetting?.repairTypeName
+  const fetchData = useCallback(
+    async (action: Function, params: Record<string, boolean | string>, loadingMessage: string) => {
+      const closeLoading = message.loading(loadingMessage)
+      try {
+        const res = await dispatch(action(params))
+        if ('error' in res && res.error?.message) {
+          throw new Error(res.error.message)
+        }
+      } catch (err: unknown) {
+        if (err instanceof Error) message.error(err.message)
+      } finally {
+        closeLoading()
+      }
     },
-    { key: 'repairName', headerName: '报修人', align: 'center' },
-    { key: 'tel', headerName: '报修人电话', align: 'center' },
-    { key: 'appointmentTime', headerName: '预约时间', align: 'center' },
-    {
-      key: 'statusCd',
-      headerName: '状态',
-      align: 'center',
-      renderCell: row =>
-        row.statusCd === 1000
-          ? '未派单'
-          : row.statusCd === 1100
-            ? '接单'
-            : row.statusCd === 1200
-              ? '退单'
-              : row.statusCd === 1300
-                ? '转单'
-                : row.statusCd === 1400
-                  ? '申请支付'
-                  : row.statusCd === 1500
-                    ? '支付失败'
-                    : row.statusCd === 1700
-                      ? '待评价'
-                      : row.statusCd === 1800
-                        ? '电话回访'
-                        : row.statusCd === 1900
-                          ? '办理完成'
-                          : row.statusCd === 2000
-                            ? '未办理结单'
-                            : row.statusCd === 2001
-                              ? '暂停'
-                              : ''
-    },
-    {
-      key: 'operate',
-      headerName: '操作',
-      align: 'center',
-      renderCell: row => {
-        const actions = [
-          {
-            title: '转单',
-            color: 'primary' as const,
-            icon: <SwapHoriz fontSize="small" />,
-            onClick: () => setTransferOpen(true)
-          },
-          {
-            title: '退单',
-            color: 'primary' as const,
-            icon: <Undo fontSize="small" />,
-            onClick: () => setChargebackOpen(true)
-          },
-          {
-            title: '办结',
-            color: 'primary' as const,
-            icon: <CheckCircle fontSize="small" />,
-            onClick: () => setConcludeOpen(true)
-          },
-          {
-            title: '暂停',
-            color: 'primary' as const,
-            icon: <PauseCircle fontSize="small" />,
-            onClick: () => setPauseOpen(true)
-          },
-          {
-            title: '启动',
-            color: 'primary' as const,
-            icon: <PlayCircle fontSize="small" />,
-            onClick: () => setActivateOpen(true)
-          },
-          {
-            title: '详情',
-            color: 'primary' as const,
-            icon: <FileCopy fontSize="small" />,
-            onClick: () => navigate('/repair/work-order-details', { state: { value: row } })
-          }
-        ]
-        const filteredActions = actions.filter(action => {
-          if (row.statusCd === 2001) {
-            return ['启动', '详情'].includes(action.title)
-          }
-          return action.title !== '启动'
-        })
-        return (
-          <Box>
-            {filteredActions.map((action, index) => (
-              <Tooltip title={action.title} key={index}>
-                <IconButton size="small" color={action.color} onClick={action.onClick}>
-                  {action.icon}
-                </IconButton>
-              </Tooltip>
-            ))}
-          </Box>
-        )
-      }
-    }
-  ]
-
-  const fetchData = useCallback(async () => {
-    const closeLoading = message.loading('正在加载列表中，请稍后...')
-    try {
-      const res = await dispatch(
-        find({ 'page.num': page.num, 'page.size': page.size, statusCd: 1100 })
-      )
-      if ('error' in res && res.error?.message) {
-        throw new Error(res.error.message)
-      }
-    } catch (err: unknown) {
-      closeLoading()
-      if (err instanceof Error) message.error(err.message)
-    } finally {
-      closeLoading()
-    }
-  }, [dispatch, page.num, page.size])
-
-  const fetchRepairSettingData = useCallback(async () => {
-    const closeLoading = message.loading('正在加载列表中，请稍后...')
-    try {
-      const res = await dispatch(
-        findRepairSetting({ 'page.num': page.num, 'page.size': page.size })
-      )
-      if ('error' in res && res.error?.message) {
-        throw new Error(res.error.message)
-      }
-    } catch (err: unknown) {
-      closeLoading()
-      if (err instanceof Error) message.error(err.message)
-    } finally {
-      closeLoading()
-    }
-  }, [dispatch, page.num, page.size])
+    [dispatch]
+  )
 
   useEffect(() => {
-    fetchData()
-    fetchRepairSettingData()
-  }, [fetchData, fetchRepairSettingData])
+    fetchData(find, { 'page.num': page.num, 'page.size': page.size }, '正在加载列表中，请稍后...')
+    fetchData(
+      findRepairSetting,
+      { 'page.num': page.num, 'page.size': page.size },
+      '正在加载列表中，请稍后...'
+    )
+  }, [fetchData, page.num, page.size])
+
+  const handleActionClick = useCallback(
+    (actionType: string, row: RepairPoolReply) => {
+      switch (actionType) {
+        case 'transferOrder':
+          setDialogValue(row)
+          setTransferOpen(true)
+          break
+        case 'cancelOrder':
+          setDialogValue(row)
+          setChargebackOpen(true)
+          break
+        case 'done':
+          setDialogValue(row)
+          setConcludeOpen(true)
+          break
+        case 'pause':
+          setDialogValue(row)
+          setPauseOpen(true)
+          break
+        case 'launch':
+          setDialogValue(row)
+          setActivateOpen(true)
+          break
+        case 'details':
+          navigate('/repair/WorkOrderDetails', { state: { value: row } })
+          break
+      }
+    },
+    [navigate]
+  )
+
+  const renderActionButtons = (row: RepairPoolReply) =>
+    [
+      { title: '转单', action: 'transferOrder' },
+      { title: '退单', action: 'cancelOrder' },
+      { title: '办结', action: 'done' },
+      { title: '暂停', action: 'pause' },
+      { title: '启动', action: 'launch' },
+      { title: '详情', action: 'details' }
+    ].map(({ title, action }) => (
+      <Chip
+        key={title}
+        sx={{
+          cursor: 'pointer',
+          marginRight: '-5px',
+          '& .MuiChip-label': {
+            fontSize: '13px'
+          }
+        }}
+        label={title}
+        color="primary"
+        variant="outlined"
+        onClick={() => handleActionClick(action, row)}
+      />
+    ))
 
   return (
     <>
-      <TableList rows={list} columns={columns} setDialogValue={setDialogValue} />
+      <DataGrid
+        sx={{ mt: 1 }}
+        localeText={zhCN.components.MuiDataGrid.defaultProps.localeText}
+        disableColumnResize
+        disableVirtualization={false}
+        rows={list}
+        columns={[
+          {
+            field: 'communityId',
+            headerName: '位置',
+            flex: 1,
+            headerAlign: 'center',
+            align: 'center',
+            renderCell: () => community.name
+          },
+          {
+            field: 'repairSetting',
+            headerName: '报修类型',
+            flex: 1,
+            headerAlign: 'center',
+            align: 'center',
+            renderCell: ({ row }) => row.repairSetting?.repairTypeName
+          },
+          {
+            field: 'repairName',
+            headerName: '报修人',
+            flex: 1,
+            headerAlign: 'center',
+            align: 'center'
+          },
+          {
+            field: 'tel',
+            headerName: '报修人电话',
+            flex: 1,
+            headerAlign: 'center',
+            align: 'center'
+          },
+          {
+            field: 'appointmentTime',
+            headerName: '预约时间',
+            flex: 1,
+            headerAlign: 'center',
+            align: 'center'
+          },
+          {
+            field: 'statusCd',
+            headerName: '状态',
+            flex: 1,
+            headerAlign: 'center',
+            align: 'center',
+            renderCell: ({ row }) => <Chip label={statusValue[row.statusCd!] || '未知'} />
+          },
+          {
+            field: 'actions',
+            headerName: '操作',
+            type: 'actions',
+            width: 300,
+            getActions: ({ row }) => renderActionButtons(row)
+          }
+        ]}
+        pageSizeOptions={[10, 20, 50, 100]}
+        paginationMode="server"
+        rowCount={Number(page.total)}
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: Number(page.size)
+            }
+          }
+        }}
+      />
       <TransferOfOrder
         dialogValue={dialogValue}
         transferOpen={transferOpen}
@@ -207,7 +213,7 @@ const TableData: React.FC<TableDataProps> = () => {
         concludeOpen={concludeOpen}
         setConcludeOpen={setConcludeOpen}
       />
-      <Pause dialogValue={dialogValue} pauseOpen={pauseOpen} setPauseOpen={setPauseOpen} />
+      <Pause dialogValue={dialogValue} pauseOpen={pauseOpen} setPauseOpen={setPauseOpen} />{' '}
       <Launch
         dialogValue={dialogValue}
         activateOpen={activateOpen}
